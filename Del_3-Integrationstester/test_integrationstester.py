dd5cd629b2388e61cb4f6216398ccd3a7e7b22a8
Ebesoh <ebesoh82@gmail.com>
2026-01-26 22:25:50 +0100

    Fault fixed:   assert isinstance(product["id"], int)

--- Del_3-Integrationstester/test_integrationstester.py
+++ Del_3-Integrationstester/test_integrationstester.py
@@ -54,7 +54,7 @@ def test_product_field_types(): # Testfall kontrollera att fälten i en product 
     response = requests.get(f"{BASE_URL}/products")
     product = response.json()[0]
 
-    assert isinstance(product["id1"], int)
+    assert isinstance(product["id"], int)
     assert isinstance(product["title"], str)
     assert isinstance(product["price"], (int, float))
     assert isinstance(product["description"], str)
